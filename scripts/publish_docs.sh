222959ab722ab182db8cc9d14f53bee3ae6b00f2
Dhuan Oliveira <dhuanco@gmail.com>
2020-12-31 23:53:21 +0100

    checkout

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -3,6 +3,10 @@ set -xe
 git config --global user.email "giback_bot@noemail.com"
 git config --global user.name "Giback Bot"
 
+cp -r ./docs/build/html ./public
+
+git checkout .
+
 git remote set-url origin https://giback:$GH_KEY_DOCS@github.com/dhuan/giback.git
 
 echo "!!!!!!!!!!!!!!!!!!!!!!"
@@ -10,8 +14,6 @@ ls -la
 git fetch
 
 git checkout gh-pages
-
-cp -r ./docs/build/html ./public
 
 git add ./public
 
