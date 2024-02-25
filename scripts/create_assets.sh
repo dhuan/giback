d7d2964ba722c296bf7776b12483a29c0d511565
Dhuan Oliveira <dhuanco@gmail.com>
2024-02-25 16:05:46 +0100

    progress

--- scripts/create_assets.sh
+++ scripts/create_assets.sh
@@ -41,7 +41,7 @@ TARGET_FOLDERS=$(ls ./release_downloads)
 for TARGET_FOLDER in ${TARGET_FOLDERS[@]}
 do
     (
-        cd "./release_downloads/${TARGET_FOLDER}"
+        cd "./release_downloads"
         tar czv ./* > "./giback_${GIBACK_VERSION}_${TARGET_FOLDER}.tgz"
     )
 done
